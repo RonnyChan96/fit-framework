296f2a626d748eeb6a2e0aee9d59abc5d73df9c7
RonnyChan <ronny_chan@126.com>
2025-09-09 23:18:57 +0800

    [elsa] Modify retrieve node update logical.

--- framework/elsa/fit-elsa-react/src/components/retrieval/KnowledgeForm.jsx
+++ framework/elsa/fit-elsa-react/src/components/retrieval/KnowledgeForm.jsx
@@ -93,11 +93,10 @@ const _KnowledgeForm = ({knowledge, groupId, knowledgeConfigId, disabled, maximu
       value: {
         selectedGroupId: groupId ?? DEFAULT_KNOWLEDGE_REPO_GROUP,
         selectedKnowledgeConfigId: knowledgeConfigId,
-        onSelect: (repoGroupId, knowledgeConfigId) => {
+        onSelect: (data) => {
           dispatch({
-            type: 'updateGroupIdAndConfigId',
-            value: repoGroupId,
-            knowledgeConfigId: knowledgeConfigId,
+            type: 'updateKnowledgeFlat',
+            ...data
           });
         },
       },
